444e420a95cd5e885a9ef800b24f48efc73568a6
Shako Yevhen <schako15101992@gmail.com>
2023-03-23 23:39:03 +0200

    Виправив медіаправила

--- JS/menu.js
+++ JS/menu.js
@@ -4,11 +4,12 @@
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+            menuBtnRef.getAttribute("aria-expanded") === 'true' || false;
 
-        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.classList.toggle("is-active");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenuRef.classList.toggle("is-open");
+        body.classList.toggle("no-scroll");
     });
 })();
